aad661d5a112b85ab4c36c2b8c15fa2e65490015
camsparky <cameronsparks@cmail.carleton.ca>
2024-02-16 14:35:46 -0500

    Writing to file within script

--- Password/firstScript.bat
+++ Password/firstScript.bat
@@ -2,7 +2,8 @@
 setlocal enabledelayedexpansion
 set _string=User=Your Friend
 echo %_string%
+copy NUL output.txt
 for /f "tokens=1 delims==" %%a in ("%_string%") do (
-  echo %%a
+  echo %%a>> output.txt
   )
 endlocal
