e6a389fdfeb2ff6dc7464db4dd69251bdad3926e
camsparky <cameronsparks@cmail.carleton.ca>
2024-02-16 14:43:22 -0500

    Bug with adding a file

--- Password/firstScript.bat
+++ Password/firstScript.bat
@@ -1,9 +1,2 @@
 @echo off
-setlocal enabledelayedexpansion
-set _string=User=Your Friend
-echo %_string%
-copy NUL output.txt
-for /f "tokens=1 delims==" %%a in ("%_string%") do (
-  echo %%a>> output.txt
-  )
-endlocal
+echo TEST!> output.txt
